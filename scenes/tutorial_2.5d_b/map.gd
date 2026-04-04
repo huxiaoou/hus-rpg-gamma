be60fc3f672742074343854447826e974072e8cb
huxiaoou <516984451@qq.com>
2026-04-04 22:09:30 +0800

    Try import .blend from blender

--- scenes/tutorial_2.5d_b/map.gd
+++ scenes/tutorial_2.5d_b/map.gd
@@ -9,21 +9,23 @@ var offset: float = 0.50 * w
 var scene_hexagon_highland: PackedScene = preload("res://scenes/tutorial_2.5d_b/hexagon_tile_highland.tscn")
 var scene_hexagon_sea: PackedScene = preload("res://scenes/tutorial_2.5d_b/hexagon_tile_sea.tscn")
 var scene_hexagon_plain: PackedScene = preload("res://scenes/tutorial_2.5d_b/hexagon_tile_plain.tscn")
+var scene_hexagon_sea2: PackedScene = preload("res://scenes/tutorial_2.5d_b/assets/hexagon.blend")
 var scenes_array: Array[PackedScene]
 
 func _ready() -> void:
     scenes_array.append(scene_hexagon_highland)
-    scenes_array.append(scene_hexagon_sea)
+    #scenes_array.append(scene_hexagon_sea)
     scenes_array.append(scene_hexagon_plain)
+    scenes_array.append(scene_hexagon_sea2)
     generate_hex_test()
     
 
 
 func generate_hex_test() -> void:
-    var total: int = 6
+    var total: int = 12
     for x: int in total:
         for z: int in total:
-            var hex: Hexgon = scenes_array[randi()%3].instantiate()
+            var hex = scenes_array[randi()% scenes_array.size()].instantiate()
             add_child(hex)
             if z % 2 == 0:
                 hex.global_position = Vector3(x * w, 0, h * z)
