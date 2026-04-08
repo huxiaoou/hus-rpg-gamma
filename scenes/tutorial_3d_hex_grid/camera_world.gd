c4e7f15e7fc763b36a22618db2954690f702058d
huxiaoou <516984451@qq.com>
2026-04-08 22:07:26 +0800

    Adjust camera movements

---
+++ scenes/tutorial_3d_hex_grid/camera_world.gd
@@ -0,0 +1,44 @@
+extends Node3D
+
+class_name CameraWorld
+
+@export_category("Camera")
+
+@export_group("Movement Speed")
+@export var speed: float = 6
+@export var rotation_speed: float = PI / 3
+@export var zoom_step: float = 0.15
+
+@export_group("Movement Boundry")
+@export var xlim: Vector2 = Vector2(-2, 72)
+@export var zlim: Vector2 = Vector2(-2, 72)
+@export var ylim: Vector2 = Vector2(2, 4)
+
+@onready var camera: Camera3D = $Camera
+
+
+func _process(delta: float) -> void:
+    # --- rotation ---
+    var rotation_direction: float = Input.get_axis("camera_rotate_clockwise", "camera_rotate_counter_clockwise")
+    rotate_y(rotation_speed * delta * rotation_direction)
+
+    # --- movement ---
+    var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_back").normalized()
+    var velocity: Vector2 = direction.rotated(-global_rotation.y) * speed * delta
+    var d: Vector3 = Vector3(velocity.x, 0, velocity.y)
+    global_position += d
+
+    clamp_position()
+
+func _unhandled_input(event: InputEvent) -> void:
+    if event.is_action_pressed("camera_zoom_out"):
+        global_position.y += zoom_step
+    elif event.is_action_pressed("camera_zoom_in"):
+        global_position.y -= zoom_step
+
+
+func clamp_position() -> void:
+    global_position.x = clamp(global_position.x, xlim.x, xlim.y)
+    global_position.z = clamp(global_position.z, zlim.x, zlim.y)
+    global_position.y = clamp(global_position.y, ylim.x, ylim.y)
+    return
