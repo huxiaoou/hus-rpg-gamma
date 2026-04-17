aa1033f7f0992a43bd0148602796a784ac508d16
huxiaoou <516984451@qq.com>
2026-04-17 22:19:05 +0800

    Add validate logic when inserting and deleting hextiles

--- scenes/tutorial_3d_hex_grid_beta/map_editor_b.gd
+++ scenes/tutorial_3d_hex_grid_beta/map_editor_b.gd
@@ -66,14 +66,21 @@ func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_pressed("confirm_hex_placement"):
         var pos: Vector3 = ManagerHextileGrid.get_xz_projection(xz_plane_y)
         var hex_coords: Vector2i = ManagerHextileGrid.point_to_hex_coordinates(pos)
+        if hex_coords in mgr_map_data.forward:
+            print("Hex already occupied at coords: ", hex_coords)
+            return
+
         var hex_pos: Vector3 = ManagerHextileGrid.hex_coordinates_to_point(hex_coords, xz_plane_y)
         var data: DataTileB = hex_tile.add_instance_at(hex_pos)
         mgr_map_data.add_pair(hex_coords, data)
     elif event.is_action_pressed("cancel_hex_placement"):
         var pos: Vector3 = ManagerHextileGrid.get_xz_projection(xz_plane_y)
         var hex_coords_to_remove: Vector2i = ManagerHextileGrid.point_to_hex_coordinates(pos)
-        var data: DataTileB = mgr_map_data.get_data_tile(hex_coords_to_remove)
+        if hex_coords_to_remove not in mgr_map_data.forward:
+            print("No hex to remove at coords: ", hex_coords_to_remove)
+            return
 
+        var data: DataTileB = mgr_map_data.get_data_tile(hex_coords_to_remove)
         var ht: HexTileB = manger_mesh[data.multi_mesh_name]
         var last_data: DataTileB = ht.remove_instance_from_index(data.id)
         var last_hex_coords: Vector2i = mgr_map_data.get_coords(last_data)
